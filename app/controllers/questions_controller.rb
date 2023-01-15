fe65552afb049ddccbe6e3d4c18215ffec37f5cd
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-01-15 15:54:19 +0600

    Add delete and first partial

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,18 +3,35 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
-  def index
-    @questions = Question.all
-  end
-
   def create
     @question = Question.create(question_params)
 
     redirect_to question_path(@question)
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.update(question_params)
+
+    redirect_to question_path(@question)
+  end
+
+  def index
+    @questions = Question.all
+  end
+
   def show
     @question = Question.find(params[:id])
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+
+    redirect_to root_path
   end
 
   private
