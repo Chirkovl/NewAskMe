839d10c966e0831c0bb4a3e3da48ff2e06550bd3
Leonid Chirkov <leonidchirkovul@gmail.com>
2023-01-17 19:55:08 +0600

    add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "questions#index"
   resources :questions
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create destroy]
+  resource :session, only: %i[new create destroy]
 end
